7dcf3219a5b5f9e7c101c52d0d9b5c5246260993
OsleneCerqueira <oslene.cerqueira@gmail.com>
2025-01-11 14:20:19 -0300

    🐛 fix: menu sobrepondo item selecionado

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const menu = document.querySelector('.menu');
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 350, 
+            behavior: 'smooth' 
+        });
+    });
+});
